3c63a4d689179f8516dac450fe134677c7699930
Аrtyom <hakishmal@yandex.ru>
2025-07-08 07:19:07 +0300

    Выполненное задание 2

--- src/main.py
+++ src/main.py
@@ -15,3 +15,16 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
         return taxed_prices
     elif isinstance(prices,int):
         return prices*tax_rate*0.01+prices
+
+
+
+def calculate_tax(price: float, tax_rate: float) -> float:
+    '''
+    Функция должна вычислять стоимость товара с учетом налога и возвращать результат
+    '''
+    if not isinstance(price, (int, float)) or price < 0:
+        raise ValueError('Неверная цена')
+    if not isinstance(tax_rate, (int, float)) or tax_rate < 0:
+        raise ValueError('Неверный налоговый процент')
+
+    return price * (1 + tax_rate / 100)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
-from src.main import calculate_taxes
+from src.main import calculate_taxes, calculate_tax
+
 import pytest
 
-@pytest.mark.parametrize('price,tax,expected', [
+@pytest.mark.parametrize('prices,taxs,expected', [
     ([10,20,30,40], 10, [11.0, 22.0, 33.0, 44.0]),
     ([10],10,[11]),
     ([100],100,[200]),
@@ -9,15 +10,33 @@ import pytest
     (100,0,100),
     (0,0,0)
 ])
-def test_calculate_taxes(price,tax,expected):
-    assert calculate_taxes(price,tax) == expected
+def test_calculate_taxes(prices,taxs,expected):
+    assert calculate_taxes(prices,taxs) == expected
 
-def test_negative_price():
+def test_negative_prices():
     with pytest.raises(ValueError) as exc_info:
         calculate_taxes([-100], 10)
     assert str(exc_info.value) == 'Неверная цена'
 
-def test_negative_tax():
+def test_negative_taxes():
     with pytest.raises(ValueError) as exc_info:
         calculate_taxes([100], -10)
+    assert str(exc_info.value) == 'Неверный налоговый процент'
+
+@pytest.mark.parametrize('price,tax,expected',[
+    (1000,1,1010),
+    (200,20,240),
+    (10,10,11),
+    (100,0,100),
+    (0,100,0)
+])
+def test_calculate_tax(price,tax,expected):
+    assert calculate_tax(price,tax) == expected
+def test_negative_price():
+    with pytest.raises(ValueError) as exc_info:
+        calculate_tax(-100, 10)
+    assert str(exc_info.value) == 'Неверная цена'
+def test_negative_tax():
+    with pytest.raises(ValueError) as exc_info:
+        calculate_tax(100, -10)
     assert str(exc_info.value) == 'Неверный налоговый процент'
